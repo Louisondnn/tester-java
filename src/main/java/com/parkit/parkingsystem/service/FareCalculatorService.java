d7e6a29dbdd6d1250e5844582ef418ccff664058
louison23 <donne.louiosn72@gmail.com>
2024-12-06 11:05:50 +0100

    reccurent + price

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -1,23 +1,8 @@
 package com.parkit.parkingsystem.service;
 
 import com.parkit.parkingsystem.constants.Fare;
-import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.model.Ticket;
 import java.time.Duration;
-import java.time.LocalDateTime;
-
- // if (ticket.getInTime() > ticket.getOutTime()) {
-        //     throw new IllegalArgumentException("In time cannot be greater than out time");
-        // }
-
-        // long inTime = ticket.getInTime().getTime();
-        // long outTime = ticket.getOutTime().getTime();
-        // System.out.println("intime" + inTime);
-        // System.out.println("out" + outTime);
-
-        // long duration = ticket.getOutTime().getTime() - ticket.getInTime().getTime();
-        // System.out.println("duration" + duration);
-        // System.out.println("duration" + duration * (1000 * 60 * 60));
 
         public class FareCalculatorService {
         
@@ -49,7 +34,11 @@ import java.time.LocalDateTime;
                         default:
                             throw new IllegalArgumentException("Unknown parking type: " + ticket.getParkingSpot().getParkingType());
                     }
-                                fare = (totalMinutes / 60) * ratePerHour; // Charge per hour
+                    fare = (totalMinutes / 60) * ratePerHour; 
+                    // Charge per hour
+                    System.out.println("Calculated fare: " + fare);
+                    System.out.println("Rate per hour: " + ratePerHour);
+
                     fare += (ratePerHour / 60.0) * (totalMinutes % 60); // Charge for remaining minutes
                 }
             
--- src/main/java/com/parkit/parkingsystem/service/RecurrentUser.java
+++ src/main/java/com/parkit/parkingsystem/service/RecurrentUser.java
@@ -2,11 +2,12 @@ package com.parkit.parkingsystem.service;
 import java.util.HashMap;
 import java.util.Map;
 
-public class RecurrentUser {
-    private Map<String, Boolean> recurrentUsers;
+
+public class RecurrentUser  {
+    private Map<String, Boolean> recurrentUsers; // Indique si l'utilisateur est récurrent
     private double normalTariff;
 
-    public RecurrentUser(double normalTariff) {
+    public RecurrentUser (double normalTariff) {
         this.recurrentUsers = new HashMap<>();
         this.normalTariff = normalTariff;
     }
@@ -14,29 +15,30 @@ public class RecurrentUser {
     public void enterGarage(String licensePlate) {
         if (recurrentUsers.containsKey(licensePlate)) {
             System.out.println("Heureux de vous revoir ! En tant qu'utilisateur régulier de notre parking, vous allez obtenir une remise de 5%");
-            recurrentUsers.put(licensePlate, true);
+            recurrentUsers.put(licensePlate, true); // Marquer comme récurrent
         } else {
-            recurrentUsers.put(licensePlate, false);
+            recurrentUsers.put(licensePlate, false); // Premier passage
+            System.out.println("Bienvenue dans notre garage !");
         }
     }
 
     public double calculateTariff(String licensePlate) {
-        if (recurrentUsers.containsKey(licensePlate)) {
-            if (recurrentUsers.get(licensePlate)) {
-                return normalTariff * 0.95; // 5% remise
-            } else {
-                return normalTariff;
-            }
+        if (recurrentUsers.containsKey(licensePlate) && recurrentUsers.get(licensePlate)) {
+            return normalTariff * 0.95; // 5% remise pour les utilisateurs récurrents
         } else {
-            return normalTariff;
+            return normalTariff; // Pas de remise pour les nouveaux utilisateurs
         }
     }
+
     public void exitGarage(String licensePlate) {
-        if (recurrentUsers.get(licensePlate)) {
-            System.out.println("Merci pour votre visite ! Vous avez obtenu une remise de 5% sur votre tarif.");
+        if (recurrentUsers.containsKey(licensePlate)) {
+            if (recurrentUsers.get(licensePlate)) {
+                System.out.println("Merci pour votre visite ! Vous avez obtenu une remise de 5% sur votre tarif.");
+            } else {
+                System.out.println("Merci pour votre visite !");
+            }
         } else {
             System.out.println("Merci pour votre visite !");
         }
-        recurrentUsers.remove(licensePlate);
     }
 }
